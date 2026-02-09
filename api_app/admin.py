9e7a9a80d3029d1ea34f0fa041cd718014b4f168
JoshBelloUni <ed20j4b@leeds.ac.uk>
2026-02-09 16:50:45 +0000

    created models for transport and reviews

--- api_app/admin.py
+++ api_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Trail, Review, TransportLink
+
+# Register your models here
+admin.site.register(Trail)
+admin.site.register(Review)
+admin.site.register(TransportLink)
